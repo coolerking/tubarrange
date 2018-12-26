46e58e684132877d54c4ddcc355a4bad36730a5b
堀 扶 <89004@ad.exa-corp.co.jp>
2018-12-26 10:20:34 +0900

    setup.py 作成

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'tubarrange',
+    version = '1.0.0',
+    author = 'Tasuku Hori',
+    author_email = 'tasuku-hori@exa-corp.co.jp',
+    url = 'https://github.com/coolerking/tubarrange',
+    install_requires = ['docopt'],
+    license = 'MIT'
+)
